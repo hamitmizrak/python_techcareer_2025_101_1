9c371496f0a931adcce18eb14af00cf2dae9222e
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-13 16:44:28 +0300

    _01_print_comment

---
+++ python_pure_tutorials/_01_print_comment.py
@@ -0,0 +1,21 @@
+# Single Comment
+
+"""
+Multiple Comment
+"""
+
+#=============================================
+# PRINT
+#=============================================
+print("Python Öğreniyorum")
+
+print("Python","Java","Nodejs")
+print("Python" "Java" "Nodejs")
+# print("Python" "Java" "Nodejs")  # YAZAMAZSINIZ
+
+# Tam sayı
+print(1453)
+
+# Floating Point
+print(14.53)
+print(14,53)
