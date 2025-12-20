cfc21b4b0ed15e6328ac360a4defb3e17843e0af
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-20 17:10:11 +0300

    SEPERATE, END, DOCSTRING, ESCAPE CHARACTER VARIABLE Formatting f Datatypes (str, number, boolean), STRING (DİZGİ) BOOLEAN (TRUE/FALSE) (Type == > list) (Type == > tuple) (Type == > set) (Type == > dict)
    GC_Cast
    Operators(aritmetik)
    Logic
    Math

---
+++ python_pure_tutorials/_05_Math.py
@@ -0,0 +1,34 @@
+#######################################################
+####### MATH ##########################################
+import math
+
+
+#######################################################
+####### SABİT #########################################
+print("PI: ", math.pi)
+print("E: ", math.e)
+
+
+#######################################################
+####### SABİT #########################################
+print("Karekök: ", math.sqrt(16))
+print("üslü: ", math.pow(2,5))
+print("üslü: ", (2**5))
+print("faktöriyel: ", math.factorial(5))
+print("Yukarı yuvarla: ", math.ceil(4.001))
+print("Aşağı yuvarla: ", math.floor(4.9))
+
+
+
+#######################################################
+####### LOGARITMIK ####################################
+print("Log: ", math.log(100))
+
+
+#######################################################
+####### LOGARITMIK ####################################
+number_list=[1,2,3,4,5]
+print("En büyük: ", max(number_list))
+print("En küçük: ", min(number_list))
+print("Sum: ", sum(number_list))
+print("Average: ", sum(number_list)/len(number_list))
