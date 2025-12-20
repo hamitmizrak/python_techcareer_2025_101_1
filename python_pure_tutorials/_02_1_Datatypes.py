2bf6304b54f71fb09303f752ad878e1dcb5193fb
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-20 15:15:21 +0300

    SEPERATE, END, DOCSTRING, ESCAPE CHARACTER VARIABLE Formatting f Datatypes (str, number, boolean)

---
+++ python_pure_tutorials/_02_1_Datatypes.py
@@ -0,0 +1,28 @@
+# pylint: disable=C0114
+""" """
+
+# Python dinamik türlü Yüksek seviyeli interpreter bir dildir.
+# Değişken türleri genel kurallar (Common Rules)
+# değişken isimlendirmelerde: _(under score)
+# snake_case
+
+################################################################################
+##### NUMBER (SAYILAR) #########################################################
+# Number
+number1 = 10  # Pozitif bir sayıdır
+number2 = -10  # Negatif bir sayıdır
+PI_NUMBER = 3.14159
+print(f"number1:  {number1}, number2:  {number2} PI_NUMBER {PI_NUMBER}")
+
+################################################################################
+##### STRING (DİZGİ) ###########################################################
+# Kelimeler
+string1 = "Merhabalar Güzel insanlar-1"
+string2 = "Merhabalar Güzel insanlar-2"
+print(f"string1:  {string1}, string2:  {string2}")
+
+################################################################################
+##### BOOLEAN (TRUE/FALSE) #####################################################
+# Boolean (True, False)
+is_login = True
+print(f"giriş yapıldı mı ?:  {is_login}")
---
+++ python_pure_tutorials/_02_2_Datatypes.py
@@ -0,0 +1,11 @@
+# pylint: disable=C0114
+""" """
+
+# Python dinamik türlü Yüksek seviyeli interpreter bir dildir.
+# Değişken türleri genel kurallar (Common Rules)
+# değişken isimlendirmelerde: _(under score)
+# snake_case
+
+################################################################################
+##### OTHER #########################################################
+
