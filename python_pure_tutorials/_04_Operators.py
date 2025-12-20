7eb41698898cacac5135df2fe8c04155c5b680f8
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-20 16:53:10 +0300

    SEPERATE, END, DOCSTRING, ESCAPE CHARACTER VARIABLE Formatting f Datatypes (str, number, boolean), STRING (DİZGİ) BOOLEAN (TRUE/FALSE) (Type == > list) (Type == > tuple) (Type == > set) (Type == > dict)
    GC_Cast
    Operators(aritmetik)
    Logic

--- python_pure_tutorials/_04_Operators.py
+++ python_pure_tutorials/_04_Operators.py
@@ -120,15 +120,30 @@ for i in range(10):
 """
 
 
-#####################################################################################
-#### Sayısal İşlemler Dört İşlem ####################################################
+# Field
 number1=13
 number2=2
+
+#####################################################################################
+#### Sayısal İşlemler Dört İşlem ####################################################
 print("Toplam: ", (number1+number2))
 print("Çıkarma: ", (number1-number2))
 print("Çarp: ", (number1*number2))
-print("Bölme: ", (number1/number2))  # Bölüm: virgüllü kalan
-print("Bölme: ", (number1//number2)) # Bölüm: tam sayılı kalan
-print("Kalan: ", (number1%number2)) # mod
-print("üslü: ", (number1**number2)) # mod
+print("Bölme: ", (number1/number2))  # Bölme: virgüllü kalan
+print("Bölme: ", (number1//number2)) # Bölme: tam sayılı kalan
+print("Kalan: ", (number1%number2))  # mod
+print("üslü: ", (number1**number2))   # mod
 
+
+#####################################################################################
+#### Logic İşlemler##################################################################
+print("Eşit mi?", (number1==number2))
+print("Eşit değil mi? ", (number1!=number2))
+print("Büyük mü? ", (number1>number2))
+print("Büyük Eşit mi? ", (number1>=number2))
+print("Küçük mü?  ", (number1<number2))
+print("Küçük Eşit mü?  ", (number1<=number2))
+
+# && = VE
+# || = VEYA
+# !  = Değil
