8138e2fdc6c92d009f8c1d6397c360eac57f0ec9
Hamit Mızrak <hamitmizrak@gmail.com>
2025-12-20 14:54:50 +0300

    SEPERATE, END, DOCSTRING, ESCAPE CHARACTER Formatting f

--- python_pure_tutorials/_01_print_comment.py
+++ python_pure_tutorials/_01_print_comment.py
@@ -4,13 +4,12 @@
 Multiple Comment
 """
 
-
-#=============================================
+# =============================================
 # PRINT
-#=============================================
+# =============================================
 print("Python Öğreniyorum")
 
-print("Python","Java","Nodejs")
+print("Python", "Java", "Nodejs")
 print("Python" "Java" "Nodejs")
 # print("Python" "Java" "Nodejs")  # YAZAMAZSINIZ
 
@@ -19,35 +18,47 @@ print(1453)
 
 # Floating Point
 print(14.53)
-print(14,53)
+print(14, 53)
 
-
-#=============================================
+# =============================================
 # SEPERATE
-#=============================================
-print("Merhabalar","Güzel","İnsanlar",sep=" * ")
+# =============================================
+print("Merhabalar", "Güzel", "İnsanlar", sep=" * ")
 
-
-#=============================================
+# =============================================
 # END
-#=============================================
-print("Merhabalar","Python","Öğreniyorum","***","Dünyasına hoşgeldiniz")
+# =============================================
+print("Merhabalar", "Python", "Öğreniyorum", "***", "Dünyasına hoşgeldiniz")
 
-print("Merhabalar","Python","Öğreniyorum", end=" *** ")
+print("Merhabalar", "Python", "Öğreniyorum", end=" *** ")
 print("Dünyasına hoşgeldiniz")
 
-
-#=============================================
+# =============================================
 # DOCSTRING
-#=============================================
+# =============================================
 print("""
 Merhabalar Python Öğreniyorum *** Dünyasına hoşgeldiniz
 """)
 
-
-#=============================================
-# ESCAPE(\) /\"' ``   \n=new
-#=============================================
+# =============================================
+# ESCAPE CHARACTER(\)    \n=new  \t=tab Formating
+# =============================================
 print("""
 Merhabalar Python Öğreniyorum *** \n\tDünyasına hoşgeldiniz
 """)
+
+# Değişken isimlendirmelerde
+name = "Hamit"
+surname = "Mızrak"
+# print("Adım: ", name," \nSoyisim: ",surname)
+
+# 1.YOL
+print("Adım: ", name," Soyisim: ",surname)
+
+# 2.YOL
+print("Adım:  %s  Soyisim:  %s "%(name,surname))
+
+# 3.YOL
+print(f"Adım:  {name}, Soyisim:  {surname}")
+
+
